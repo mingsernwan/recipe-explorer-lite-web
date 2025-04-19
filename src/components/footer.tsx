3e81e6b9ebb676638f70ee27e3509e2becc9cba4
wms <58557598+mingsernwan@users.noreply.github.com>
2025-04-19 16:55:29 +0800

    Completed: Implementing Navbar and Footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <div className="w-full bg-black h-10 flex flex-row gap-4 items-center px-10">
+      <p className="text-white text-xs">© All rights reserved</p>
+    </div>
+  );
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,18 @@
+import { ChefHatIcon } from "lucide-react";
+import Link from "next/link";
+
+export function NavBar() {
+  return (
+    <div className="bg-black h-10 flex flex-row gap-4 items-center px-10">
+      <Link href="/">
+        <ChefHatIcon className="size-5 text-white" />
+      </Link>
+      <Link className="text-white" href="/recipes">
+        Recipes
+      </Link>
+      <Link className="text-white" href="/feedback">
+        Feedback
+      </Link>
+    </div>
+  );
+}
