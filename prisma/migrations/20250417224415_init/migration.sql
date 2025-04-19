f443fc2269bcebe16bcd442c405eeeffc5f01848
wms <58557598+mingsernwan@users.noreply.github.com>
2025-04-19 17:48:45 +0800

    Completed: Redeploying dB in Neon

--- prisma/migrations/20250417224415_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Feedback" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "remarks" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
